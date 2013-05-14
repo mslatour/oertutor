108db5ae42c430c135a8843be4524f4240c51dcf
Sander Latour <sanderlatour@gmail.com>
2013-05-14 16:13:45 +0200

    Update models.py
    Started defining Exercises and ExerciseResults

--- oertutor/tutor/models.py
+++ oertutor/tutor/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class Exercise(models.Model):
+    title = models.CharField(max_length=50)    
+
+class ExerciseResult(models.Model):
+    exercise = models.ForeignKey(Exercise)
+    student = models.ForeignKey(Student)
