187ce4a2bd641da0c896c5ae4ab8261928220a0a
Sander Latour <sanderlatour@gmail.com>
2013-05-19 15:10:09 +0300

    Added OER model

--- oertutor/tutor/models.py
+++ oertutor/tutor/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from oertutor.rl.models import Action as RLAction
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class OpenEducationalResource(RLAction):
+    title = models.CharField(max_length=50)
+    source = models.URLField()
 
 class Exercise(models.Model):
     title = models.CharField(max_length=50)    
