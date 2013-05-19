dfbf892d1d8b7f50a9f34fcb88f811195c885e32
Sander Latour <sanderlatour@gmail.com>
2013-05-19 15:06:16 +0300

    Added RL Action model

--- oertutor/rl/models.py
+++ oertutor/rl/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Action(models.Model):
+    class Meta:
+        abstract = True
+
 class QValue(models.Model):
     state = models.BigIntegerField(primary_key=True)
     action = models.IntegerField()
