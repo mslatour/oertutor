283a5d65aeb1ccd051cd894415c4b384d7a7d57d
mslatour <sanderlatour@gmail.com>
2013-05-21 11:51:20 +0200

    Added human input method and others

--- oertutor/static/js/nim.js
+++ oertutor/static/js/nim.js
@@ -29,6 +29,10 @@ NimGame = function(opt = {}){
         _self.play()
     }
 
+    end_turn = function(){
+        _self.turn *= -1;
+    }
+
     play = function(){
         // Check if game is not already finished, if so: exec callback and exit
         if(_self.is_finished()){
@@ -36,12 +40,17 @@ NimGame = function(opt = {}){
             return;
         }
 
+        // Define the callback function for the input method
+        input_cb = function(stack, reduction){
+            _self.apply(_self.turn, stack, reduction)
+            _self.end_turn();
+        }
         // If it is the human player's turn
         if(_self.turn == 1){
-         
+            _self.get_human_input(_self.stacks,input_cb);
         // If it is the AI player's turn
         }else{
-
+            _self.get_ai_input(_self.stacks,input_cb);
         }
     }
 
@@ -73,5 +82,62 @@ NimGame = function(opt = {}){
         _self.stacks[stack] -= reduction
     }
 
+    // Gets input from the ai player for the next move
+    get_ai_input = function(stacks, cb){
+        return false;
+    }
+
+    // Gets input from the human player for the next move
+    // The human player is requested to enter a valid stack number
+    //   and a valid number to reduce the stack with.
+    // If there is only one option the choice will be made automatically.
+    get_human_input = function(stacks, cb){
+        if( stacks.length > 1 ){
+            stack = NaN;
+            do{
+                if( stacks.length > 2 ){
+                    msg = "Choose a nonempty stack [1-"+stacks.length+"]"
+                }else{
+                    msg = "Choose a nonempty stack [1 or 2]"
+                }
+                stack = parseInt(prompt(msg))-1;
+                if(
+                    stack == NaN ||
+                    stack < 0 ||
+                    stack >= stacks.length ||
+                    stacks[stack] == 0
+                ){
+                    alert("You have to enter the number of a nonempty stack");
+                    stack = NaN;
+                }
+            }while(stack == NaN);
+        }else{
+            stack = 0;
+        }
+        if(stacks[stack] == 1){
+            reduction = 1;
+        }else{
+            reduction = NaN;
+            do{
+                if(stacks[stack] == 2){
+                    msg = "How much do you want to take from stack "+
+                        stack+"? [1 or 2]";
+                }else{
+                    msg = "How much do you want to take from stack "+
+                        stack+"? [1-"+stacks.length+"]";
+                }
+                reduction = parseInt(prompt(msg));
+                if(
+                    reduction == NaN || 
+                    reduction < 1 || 
+                    reduction > stacks[stack]
+                ){
+                    alert("You have to enter a valid number to reduce the stack");
+                    reduction = NaN;
+                }
+            }while(reduction == NaN);
+        }
+        cb(stack, reduction);
+    }
     init(opt);
 }
