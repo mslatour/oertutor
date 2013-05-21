34cea378140bfd5db4bc340d180d4344399588a1
mslatour <sanderlatour@gmail.com>
2013-05-21 23:25:21 +0200

    Moved towards a more visual input of the human player. The stack is now selected by clicking on a graphical representation of that stack. The reduction is still entered using a javascript prompt.

--- oertutor/static/js/nim.js
+++ oertutor/static/js/nim.js
@@ -28,7 +28,8 @@ function NimGame(opt){
                 _s[option] = opt[option]
             }
         }
-        _s.play()
+        _s.init_render();
+        _s.play();
     }
 
     _s.end_turn = function(){
@@ -39,11 +40,9 @@ function NimGame(opt){
         // Check if game is not already finished, if so: exec callback and exit
         if(_s.is_finished()){
             _s.cb_finished(-1*_s.turn, _s.playback);
+            _s.render();
             return;
         }
-
-        // Render current state
-        _s.render();
 
         // Define the callback function for the input method
         input_cb = function(stack, reduction){
@@ -53,10 +52,10 @@ function NimGame(opt){
         }
         // If it is the human player's turn
         if(_s.turn == 1){
-            _s.get_human_input(_s.stacks,input_cb);
+            _s.get_human_input(input_cb);
         // If it is the AI player's turn
         }else{
-            _s.get_ai_input(_s.stacks,input_cb);
+            _s.get_ai_input(input_cb);
         }
     }
 
@@ -69,22 +68,70 @@ function NimGame(opt){
         return true
     }   
 
+    // Initialize rendering
+    _s.init_render = function(){
+        // Creating div to put log message in
+        var logger = document.createElement("div");
+        logger.id = "nim-logger";
+        // Append logger to the output container
+        _s.container.appendChild(logger);
+        
+        // Create a container for the stacks
+        var stackcontainer = document.createElement("div");
+        stackcontainer.id = "nim-stack-container";
+        stackcontainer.className = "nim stackcontainer";
+        _s.container.appendChild(stackcontainer);
+    }
+
     // Render the game state
-    _s.render = function(){
-        body = "";
+    _s.render = function(clickcb){
+        // Generating log message;
+        log = "";
         for(var i = 0; i < _s.playback.length; i++){
             p = _s.playback[i];
-            body += "| "+p.stacks.join(" | ")+" | <b>Agent "+p.agent+
+            log += "| "+p.stacks.join(" | ")+" | <b>Agent "+p.agent+
                 " reduced stack "+(p.stack+1)+" with "+p.reduction+"</b><br />" ;
         }
-        _s.container.innerHTML = body;
+        // Add log message to logger
+        document.getElementById("nim-logger").innerHTML = log;
+
+        // Clear container for the stacks
+        var stackcontainer = document.getElementById("nim-stack-container");
+        stackcontainer.innerHTML = "";
+
+        var stack, style;
+        // Generate the stacks
+        for(var i = 0; i < _s.stacks.length; i++){
+            stack = document.createElement("div");
+            if(_s.stacks[i] == 0){
+                stack.className= "nim stack empty";
+            }else{
+                stack.className= "nim stack";
+            }
+            style = "height: "+(20+_s.stacks[i]*20)+"px;"+
+                "left: "+(10+50*i)+"px;";
+            stack.setAttribute("style", style);
+            if(_s.stacks[i] > 0 && clickcb != undefined){
+                // Create onclick action for stack.
+                // It is necessary to create a new closure
+                //   in which the current value of i will be kept.
+                stack.addEventListener('click',function(stack){
+                    return function(e){
+                        clickcb(stack);
+                    };
+                }(i));
+            }
+            stack.innerHTML = _s.stacks[i];
+            // Add stack to stack container
+            stackcontainer.appendChild(stack);
+        }
     }
 
     // Apply the selected move
     _s.apply = function(agent, stack, reduction){
         _s.playback.push({
             "agent" : agent,
-            "stacks" : _s.stacks,
+            "stacks" : _s.stacks.slice(0), // .slice(0) clones array
             "stack": stack,
             "reduction": reduction
         });
@@ -92,7 +139,7 @@ function NimGame(opt){
     }
 
     // Gets input from the ai player for the next move
-    _s.get_ai_input = function(stacks, cb){
+    _s.get_ai_input = function(cb){
         // Declare placeholder for non-empty stacks
         var ne_stacks = new Array();
 
@@ -101,10 +148,10 @@ function NimGame(opt){
         // Calculate the nimsum of the stacks
         //   and store the indices of nonempty stacks
         var nimsum_stack = 0;
-        for(var i = 0; i < stacks.length; i++){
-            if(stacks[i] > 0){
+        for(var i = 0; i < _s.stacks.length; i++){
+            if(_s.stacks[i] > 0){
                 ne_stacks.push(i);
-                nimsum_stack ^= stacks[i];
+                nimsum_stack ^= _s.stacks[i];
             }
         }
         // General case, use the nimsum operator to determine
@@ -114,8 +161,8 @@ function NimGame(opt){
         for(var i = 0; i < ne_stacks.length; i++){
             stack = ne_stacks[i];
             // If the nimsum operator 
-            if((stacks[stack]^nimsum_stack) < stacks[stack]){
-                reduction = stacks[stack] - (stacks[stack]^nimsum_stack);
+            if((_s.stacks[stack]^nimsum_stack) < _s.stacks[stack]){
+                reduction = _s.stacks[stack] - (_s.stacks[stack]^nimsum_stack);
                 cb(stack, reduction);
                 return;
             }
@@ -129,9 +176,9 @@ function NimGame(opt){
         var index = Math.floor(Math.random()*ne_stacks.length)
         stack = ne_stacks[index];
         if(ne_stacks.length % 2 == 0){
-            reduction = (stacks[stack]>1?stacks[stack]-1:1);
+            reduction = ( _s.stacks[stack] > 1 ? _s.stacks[stack]-1 : 1 );
         }else{
-            reduction = stacks[stack];
+            reduction = _s.stacks[stack];
         }
         cb(stack, reduction);
         return;
@@ -141,72 +188,40 @@ function NimGame(opt){
     // The human player is requested to enter a valid stack number
     //   and a valid number to reduce the stack with.
     // If there is only one option the choice will be made automatically.
-    _s.get_human_input = function(stacks, cb){
-        if( stacks.length > 1 ){
-            stack = NaN;
-            do{
-                if( stacks.length > 2 ){
-                    msg = "Choose a nonempty stack [1-"+stacks.length+"]"
-                }else{
-                    msg = "Choose a nonempty stack [1 or 2]"
-                }
-                msg += "\nStacks content: | "+stacks.join(" | ")+" |";
-                input = prompt(msg);
-                if(input == null){
-                    stop = confirm("Would you like to forfeit?");
-                    if(stop){
-                        return;
+    _s.get_human_input = function(cb){
+        clickcb = function(stack){
+            var reduction;
+            if(_s.stacks[stack] == 1){
+                reduction = 1;
+            }else{
+                reduction = NaN;
+                do{
+                    if(_s.stacks[stack] == 2){
+                        msg = "How much do you want to take from stack "+
+                            (stack+1)+"? [1 or 2]";
                     }else{
-                        input = NaN;
+                        msg = "How much do you want to take from stack "+
+                            (stack+1)+"? [1-"+_s.stacks[stack]+"]";
                     }
-                }
-                stack = parseInt(input)-1;
-                if(
-                    isNaN(stack) ||
-                    stack < 0 ||
-                    stack >= stacks.length ||
-                    stacks[stack] == 0
-                ){
-                    alert("You have to enter the number of a nonempty stack");
-                    stack = NaN;
-                }
-            }while(isNaN(stack));
-        }else{
-            stack = 0;
-        }
-        if(stacks[stack] == 1){
-            reduction = 1;
-        }else{
-            reduction = NaN;
-            do{
-                if(stacks[stack] == 2){
-                    msg = "How much do you want to take from stack "+
-                        (stack+1)+"? [1 or 2]";
-                }else{
-                    msg = "How much do you want to take from stack "+
-                        (stack+1)+"? [1-"+stacks[stack]+"]";
-                }
-                input = prompt(msg);
-                if(input == null){
-                    stop = confirm("Would you like to forfeit?");
-                    if(stop){
-                        return;
-                    }else{
-                        input = NaN;
+                    input = prompt(msg);
+
+                    if(input == null) return false;
+
+                    reduction = parseInt(input);
+                    if(
+                        isNaN(reduction) || 
+                        reduction < 1 || 
+                        reduction > _s.stacks[stack]
+                    ){
+                        alert("You have to enter a valid number to reduce the stack");
+                        reduction = NaN;
                     }
-                }
-                reduction = parseInt(input);
-                if(
-                    isNaN(reduction) || 
-                    reduction < 1 || 
-                    reduction > stacks[stack]
-                ){
-                    alert("You have to enter a valid number to reduce the stack");
-                    reduction = NaN;
-                }
-            }while(isNaN(reduction));
-        }
-        cb(stack, reduction);
+                }while(isNaN(reduction));
+            }
+            cb(stack, reduction);
+            return true;
+        };
+        _s.render(clickcb);
     }
 
     //Initialize the game
