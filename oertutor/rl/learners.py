803f4c26ba73775c494a2c825ad0cbd696d9b1d0
mslatour <sanderlatour@gmail.com>
2013-06-02 19:45:19 +0200

    1) renamed to QLearner.get_max_q_value 2) using django get_or_create function instead of both a get and a create in QLearner.q 3) Stopped QValue.state from being a primary key (BAD IDEA, REALLY) 4) Now using db concurrency library and added manual rollback on conflict in QLearner.update 5) Moved to new version of django in requirements

--- oertutor/rl/learners.py
+++ oertutor/rl/learners.py
@@ -1,5 +1,8 @@
+from django.db.models import Max
+from django.db import connection
 from oertutor.rl.models import QValue
 from concurrency.exceptions import RecordModifiedError
+from time import sleep
 
 class QLearner:
     def __init__(self, learning_rate, discount):
@@ -7,16 +10,24 @@ class QLearner:
         self.gamma = discount
 
     def q(self, state, action):
-        return QValue.objects.get(state__exact=state, action__exact=action)
-    
-    def get_q_max_a(self, state):
-        res = QValue.objects.filter(state__exact=state).aggregate(Max('value'))
-        return res['value__max']
+        # Return Q(s,a) if exists, otherwise create it with Q(s,a) = 0
+        obj, created = QValue.objects.get_or_create(state=state, \
+                                                    action=action, \
+                                                    defaults={'value':0.0})
+        return obj
+
+    def get_max_q_value(self, state):
+        # Find the maximum value given the state s, i.e. max_a[Q(s,a)]
+        Qs = QValue.objects.filter(state__exact=state).annotate(Max('value'))
+        if len(Qs) == 0:
+            return 0
+        else:
+            return Qs[0].value__max
 
     def update(self, state, action, reward, state_prime):
         # est_v is short for estimated value [in s']
         #   current implemented as max_a[ Q(s',a) ]
-        est_v = self.get_q_max_a(state_prime).value
+        est_v = self.get_max_q_value(state_prime)
 
         # The following loop is necessary for dealing with potential
         #  concurrency issues that can occur when multiple agents
@@ -33,5 +44,7 @@ class QLearner:
                 q.save()
                 saved = True
             except RecordModifiedError:
-                pass
-
+                # Do hard rollback and try again
+                cursor = connection.cursor()
+                cursor.execute("ROLLBACK")
+                del q
--- oertutor/rl/models.py
+++ oertutor/rl/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from concurrency.fields import IntegerVersionField
+from concurrency.fields import AutoIncVersionField
 
 class Action(models.Model):
     class Meta:
         abstract = True
 
 class QValue(models.Model):
-    version = IntegerVersionField() # to identify concurrency issues
-    state = models.BigIntegerField(primary_key=True)
+    state = models.BigIntegerField()
     action = models.IntegerField()
     value = models.FloatField()
+    version = AutoIncVersionField() # to identify concurrency issues
 
     def __cmp__(self, other):
         return self.value.__cmp__(other)
 
     def __str__(self):
-        return "<%s,%d,%f>" % (self.state, self.action, self.value)
+        return "<%d,%d,%f>" % (self.state, self.action, self.value)
 
     def __repr__(self):
         return self.__str__()
