7483e77752309dffc3eb5f61f41f78824212ad55
mslatour <sanderlatour@gmail.com>
2013-11-04 10:20:15 +0100

    Extended the nim curriculum

--- oertutor/curricula/nim.py
+++ oertutor/curricula/nim.py
@@ -2,7 +2,11 @@ from oertutor.tutor.models import Curriculum, KnowledgeComponent, TutorMove, Res
 
 def load_db():
     # Register curriculum
-    curr, created = Curriculum.objects.get_or_create(title = "Nim")
+    curr, created = Curriculum.objects.get_or_create(
+        title = "Nim",
+        description = "In this topic you will learn everything that you need" +
+            " to be a champion at the mathematical game NIM"
+    )
 
     # Register tutor moves
     elicit, created = TutorMove.objects.get_or_create(title="Elicit")
@@ -50,7 +54,7 @@ def load_db():
 
     # Save knowledge components
     for kc in kcs:
-        kc.save()
+        kcs[kc].save()
 
     # Create structure in the curriculum
     kcs['intuition'].antecedents.add(kcs['rules'])
@@ -61,4 +65,41 @@ def load_db():
 
     # Save structure
     for kc in kcs:
-        kc.save()
+        kcs[kc].save()
+
+    # Create resources
+    resource, created = Resource.objects.get_or_create(
+        title = "Rules of NIM game",
+        source = "/static/html/oer1.html",
+        tm = tell,
+        kc = kcs['rules']
+    )
+    if created:
+        resource.save()
+
+    resource, created = Resource.objects.get_or_create(
+        title = "NIM rules",
+        source = "/static/html/oer4.html",
+        tm = tell,
+        kc = kcs['rules']
+    )
+    if created:
+        resource.save()
+
+    resource, created = Resource.objects.get_or_create(
+        title = "Play a game",
+        source = "/static/html/oer5.html",
+        tm = elicit,
+        kc = kcs['intuition']
+    )
+    if created:
+        resource.save()
+
+    resource, created = Resource.objects.get_or_create(
+        title = "Example of pair canceling",
+        source = "/static/html/oer3.html",
+        tm = tell,
+        kc = kcs['cancel2powers']
+    )
+    if created:
+        resource.save()
