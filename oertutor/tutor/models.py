e93b94ba9e127a250a251aa14bca0e03b48a7826
mslatour <sanderlatour@gmail.com>
2013-11-04 10:28:37 +0100

    Added models for annotated curriculum and observations

--- oertutor/tutor/models.py
+++ oertutor/tutor/models.py
@@ -1,17 +1,39 @@
 from django.db import models
-from oertutor.rl.models import Action as RLAction
+from oertutor.rl.models import Action
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
-class OpenEducationalResource(RLAction):
+class Curriculum(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+class KnowledgeComponent(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    curriculum = models.ForeignKey(Curriculum, related_name='kcs')
+    antecedents = models.ManyToManyField('self', null=True,
+            symmetrical=False, related_name='consequent')
+
+class TutorMove(Action):
+    title = models.CharField(max_length=50)
+
+class Resource(Action):
     title = models.CharField(max_length=50)
     source = models.URLField()
+    tm = models.ForeignKey(TutorMove, related_name='resources')
+    kc = models.ForeignKey(KnowledgeComponent, related_name='resources')
 
-class Exercise(models.Model):
-    title = models.CharField(max_length=50)    
+class Exercise(Resource):
+    params = models.CharField(max_length=255)
 
 class ExerciseResult(models.Model):
-    exercise = models.ForeignKey(Exercise)
+    exercise = models.ForeignKey(Exercise, related_name='result')
     student = models.ForeignKey(Student)
+    data = models.TextField()
+
+class Observation(models.Model):
+    handle = models.CharField(max_length=50)
+    value = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now = True)
