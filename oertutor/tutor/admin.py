75296cbc6a6d98f43e2e24b548c9cdd8082b0e1c
mslatour <sanderlatour@gmail.com>
2014-02-23 19:08:08 +0100

    Added tutor models to admin

---
+++ oertutor/tutor/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from oertutor.tutor.models import *
+
+admin.site.register(Curriculum)
+admin.site.register(KnowledgeComponent)
+admin.site.register(StudentCategory)
+admin.site.register(Resource)
+admin.site.register(Test)
+admin.site.register(Question)
