f3572e801f1c9e719e2dd4bd127f45b86c1f6343
mslatour <sanderlatour@gmail.com>
2014-04-27 01:10:19 +0200

    Altered settings to work at localhost

--- oertutor/settings.py
+++ oertutor/settings.py
@@ -2,7 +2,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 import os
 # Django settings for oertutor project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/sander/projects/thesis/framework/staticfiles/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
