2c888971402b586432a321839cab5a1754aa81f3
mslatour <sanderlatour@gmail.com>
2014-05-18 15:40:07 +0200

    Set debug setting back to false

--- oertutor/settings.py
+++ oertutor/settings.py
@@ -2,7 +2,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 import os
 # Django settings for oertutor project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
